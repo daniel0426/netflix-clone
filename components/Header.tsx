fb33b712bf4aaadbcfa07d69de966c2af3ecaf75
Daniel <angus9209@gmail.com>
2022-07-19 10:44:20 +1200

    header component finished

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,58 @@
+import { BellIcon, SearchIcon } from '@heroicons/react/solid';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+
 function Header() {
-  return <div>Header</div>;
+  const [isScrolled, setIsScrolled] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+  return (
+    <header className={`${isScrolled && 'bg-[#141414'} `}>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <img
+          src="https://rb.gy/ulxxee"
+          alt="logo"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+        <p className="md:hidden px-2 py-[0.5px] border-2 rounded">Browse</p>
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">TV Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
+        </ul>
+      </div>
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <SearchIcon className="hidden h-6 w-6 sm:inline" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="h-6 w-6" />
+        <Link href="/account">
+          <img
+            src="https://rb.gy/g1pwyx"
+            alt="account logo"
+            className="cursor-pointer rounded "
+          />
+        </Link>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,20 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Header from '../components/Header';
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="relative h-screen bg-gradient-to-b from-gray-600/20 to-[#010511] lg:h-[140vh]">
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* Header */}
+      <Header />
       <main>
-        <section>
-
-        </section>
+        <section></section>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
