ca8e19b3ef7ee447237ef3150b6d57f79cdc3dcd
Daniel <angus9209@gmail.com>
2022-07-19 21:49:58 +1200

    fixed gradient background tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient-to-bottom':
+          'linear-gradient(to bottom, rgba(20,20,20,0) 0, rgba(20,20,20,.15) 15%,rgba(20,20,20,.35) 30%, rgba(20,20,20,.58) 45%, #141414 68%, #141414 100%)',
+      },
+    },
   },
   plugins: [
     require('tailwindcss-textshadow'),
