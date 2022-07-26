2794be40fb11f3515cf1112f18d3071ac9f8ab79
Daniel <angus9209@gmail.com>
2022-07-26 20:28:11 +1200

    get the movie trailer for each thumbnails

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image';
+import { useRecoilState } from 'recoil';
+import { modalState, movieState } from '../atoms/modalAtom';
 import { thumbnailBaseUrl } from '../constants/movie';
 import { Movie } from '../types';
 
@@ -7,9 +9,16 @@ interface ThumbnailProps {
 }
 
 function Thumbnail({ movie }: ThumbnailProps) {
+  const [showModal, setShowModal] = useRecoilState(modalState);
+  const [currentMovie, setCurrentMovie] = useRecoilState(movieState);
+
   return (
     <div className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105">
       <Image
+        onClick={() => {
+          setCurrentMovie(movie);
+          setShowModal(true);
+        }}
         src={`${thumbnailBaseUrl}${movie.backdrop_path || movie.poster_path}`}
         className="rounded-sm object-cover md:rounded"
         layout="fill"
