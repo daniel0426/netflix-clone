d76a65c273e5f1652218cd16fc19d72208f7ea48
Daniel <angus9209@gmail.com>
2022-07-27 15:59:15 +1200

    fix: refactoring next.config file for stripe transpile

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const withTM = require('next-transpile-modules')([
+  '@stripe/firestore-stripe-payments',
+]);
+
+module.exports = withTM({
   reactStrictMode: true,
   images: {
     domains: ['image.tmdb.org', 'rb.gy'],
   },
-};
+});
  
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,19 @@
 import Head from 'next/head';
+
 import { useRecoilValue } from 'recoil';
 import { modalState } from '../atoms/modalAtom';
+
+import useAuth from '../hooks/useAuth';
+import { Movie } from '../types';
+import apiRequests from '../utils/apiRequests';
+import { getProducts, Product } from '@stripe/firestore-stripe-payments';
+import payments from '../lib/stripe';
+
 import Banner from '../components/Banner';
 import Header from '../components/Header';
 import Modal from '../components/Modal';
 import Plans from '../components/Plans';
 import Row from '../components/Row';
-import useAuth from '../hooks/useAuth';
-import { Movie } from '../types';
-import apiRequests from '../utils/apiRequests';
 
 interface Props {
   netflixOriginals: Movie[];
@@ -19,6 +24,7 @@ interface Props {
   horrorMovies: Movie[];
   romanceMovies: Movie[];
   documentaries: Movie[];
+  products: Product[];
 }
 
 const Home = ({
@@ -30,11 +36,12 @@ const Home = ({
   horrorMovies,
   romanceMovies,
   documentaries,
+  products,
 }: Props) => {
   const { logout, loading } = useAuth();
   const showModal = useRecoilValue(modalState);
   const subscription = false;
-
+  console.log(products);
   if (loading || subscription === null) return null;
 
   if (!subscription) return <Plans />;
@@ -70,6 +77,13 @@ const Home = ({
 export default Home;
 
 export const getServerSideProps = async () => {
+  const products = await getProducts(payments, {
+    includePrices: true,
+    activeOnly: true,
+  })
+    .then((res) => res)
+    .catch((err) => console.log(err.message));
+
   const [
     netflixOriginals,
     trendingNow,
@@ -100,6 +114,7 @@ export const getServerSideProps = async () => {
       horrorMovies: horrorMovies.results,
       romanceMovies: romanceMovies.results,
       documentaries: documentaries.results,
+      products,
     },
   };
 };
